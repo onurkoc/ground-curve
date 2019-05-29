9b6a10d1f502a51f2699a7571e9278ebbe13d6db
Onur Koc <onur.koc@outlook.com>
2019-05-29 17:13:52 +0200

    change from subplot to 4 plots

--- Draw_graph.py
+++ Draw_graph.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import plotly.plotly as py
 import plotly.graph_objs as go
-from plotly import tools
 
 
 def draw(x1, y1,
@@ -24,7 +22,8 @@ def draw(x1, y1,
         line=dict(
             color='blue'
         ),
-        # hoverinfo='none',
+        xaxis='x1',
+        yaxis='y1'
     )
 
     trace1 = go.Scatter(
@@ -35,7 +34,8 @@ def draw(x1, y1,
         line=dict(
             color='red'
         ),
-        # hoverinfo='none'
+        xaxis='x1',
+        yaxis='y1'
     )
 
     trace2 = go.Scatter(
@@ -46,21 +46,10 @@ def draw(x1, y1,
         marker=dict(
             size=7,
             color='green'
-        )
+        ),
+        xaxis='x1',
+        yaxis='y1'
     )
-
-    fig = tools.make_subplots(rows=2, cols=2,
-                              shared_xaxes=True,
-                              shared_yaxes=False,
-                              vertical_spacing=0.07,
-                              horizontal_spacing=0.07,
-                              # subplot_titles=('Ground Reaction Curve',
-                              #                 'Displacement Curve')
-                              )
-
-    fig.append_trace(trace0, 1, 1)
-    fig.append_trace(trace1, 1, 1)
-    fig.append_trace(trace2, 1, 1)
 
     trace3 = go.Scatter(
         x=x4,
@@ -71,28 +60,16 @@ def draw(x1, y1,
             color='blue',
             width=1.5,
             dash='dashdot'
-        )
+        ),
+        xaxis='x1',
+        yaxis='y2'
     )
 
-    fig.append_trace(trace3, 2, 1)
+    data = [trace0, trace1, trace2, trace3]
 
     shapes = list()  # append the vertical lines later
 
     if flag is not None:
-        # this trace is not needed anymore
-        # trace4 = go.Scatter(
-        #     x=x5,
-        #     y=y5,
-        #     mode='lines',
-        #     line=dict(
-        #         color='green',
-        #         width=1.5,
-        #         dash='dashdot'
-        #     ),
-        #     name='Equil. LDP',
-        #     showlegend=False
-        # )
-        # fig.append_trace(trace4, 2, 1)
         for index, item in enumerate(x6):
             if index % 30 == 0:
                 trace5 = go.Scatter(
@@ -109,9 +86,11 @@ def draw(x1, y1,
                     legendgroup="group",
                     hoverinfo='none',
                     name='Diff. p_i',
-                    opacity=0.4
+                    opacity=0.4,
+                    xaxis='x1',
+                    yaxis='y2'
                 )
-                fig.append_trace(trace5, 2, 1)
+                data.append(trace5)
 
         trace5 = go.Scatter(
             x=x6[-1],
@@ -127,15 +106,17 @@ def draw(x1, y1,
             legendgroup="group",
             hoverinfo='none',
             name='Diff. p_i',
-            opacity=1
+            opacity=1,
+            xaxis='x1',
+            yaxis='y2'
         )
-        fig.append_trace(trace5, 2, 1)
+        data.append(trace5)
 
         # vertical lines
         for i in (x2[0], x3[0]):
             shapes.append({'type': 'line',
-                           'xref': 'x',
-                           'yref': 'y3',
+                           'xref': 'x1',
+                           'yref': 'y2',
                            'x0': i,
                            'y0': 0,
                            'x1': i,
@@ -155,9 +136,11 @@ def draw(x1, y1,
             line=dict(
                 color='red',
                 width=2
-            )
+            ),
+            xaxis='x1',
+            yaxis='y2'
         )
-        fig.append_trace(trace6, 2, 1)
+        data.append(trace6)
 
     trace7 = go.Scatter(
         x=x8,
@@ -166,9 +149,10 @@ def draw(x1, y1,
         name='SCL (NL)',
         line=dict(
             color='red'
-        )
+        ),
+        xaxis='x2',
+        yaxis='y3'
     )
-    fig.append_trace(trace7, 1, 2)
 
     trace8 = go.Scatter(
         x=x9,
@@ -177,9 +161,10 @@ def draw(x1, y1,
         name='Flow Rate',
         line=dict(
             color='blue'
-        )
+        ),
+        xaxis='x2',
+        yaxis='y4'
     )
-    fig.append_trace(trace8, 2, 2)
 
     trace9 = go.Scatter(
         x=x10,
@@ -188,9 +173,12 @@ def draw(x1, y1,
         name='Sigma (NL)',
         line=dict(
             color='red'
-        )
+        ),
+        xaxis='x2',
+        yaxis='y4'
     )
-    fig.append_trace(trace9, 2, 2)
+
+    data.extend([trace7, trace8, trace9])
 
     layout = go.Layout(
         plot_bgcolor='#f9f7f7',
@@ -211,36 +199,45 @@ def draw(x1, y1,
         height=800,
         xaxis=dict(
             rangemode='normal',
-            titlefont=dict(size=14),
             title='Tunnel Wall Displacement [m]',
-            side='bottom',
-            range=[0, max(x1)]
+            range=[0, max(x1)],
+            domain=[0, 0.47],
+            anchor='y2'
         ),
         yaxis=dict(
             scaleratio=0.1,
             tickformat='.2f',
             title='Support Pressure [MPa]',
             titlefont=dict(size=14),
-            range=[0, max(y1)]
+            range=[0, max(y1)],
+            domain=[0.55, 1]
         ),
         xaxis2=dict(
-            side='bottom',
             title='Time [days]',
+            tickformat='d',
+            domain=[0.55, 1],
+            anchor='y4'
         ),
         yaxis2=dict(
-            title='Support Pressure Pi [MPa]',
-            tickformat='.2f',
-            range=[0, max(y1)]
+            title='Distance from Tunnel Face [m]',
+            tickformat='d',
+            anchor='x1',
+            range=[80, -25],
+            domain=[0, 0.50]
         ),
         yaxis3=dict(
-            autorange='reversed',
-            title='Distance from Tunnel Face [m]',
-            range=[80, -25]
+            title='Support Pressure [MPa]',
+            tickformat='.2f',
+            range = [0, max(y1)],
+            domain=[0.55, 1],
+            anchor='x2'
         ),
         yaxis4=dict(
-            title='Stress SCL [MPa]',
+            title='Stress SpC [MPa]',
             tickformat='.2f',
-            # range=[0, max(y10)]
+            domain=[0, 0.50],
+            anchor='x2',
+            rangemode='nonnegative'
         ),
         legend=dict(
             traceorder='normal',
@@ -256,32 +253,6 @@ def draw(x1, y1,
         shapes=shapes
     )
 
-    fig['layout'].update(layout)
+    fig = go.Figure(data=data, layout=layout)
 
     return fig
-
-
-if __name__ == '__main__':
-    from plotly.offline import plot
-    from Ground_Curve import ground_curve as gc
-    values = gc()
-    if len(values) == 13:
-        p1, p2, p2_el, p3, p3_el, p4, p5, p6, v1, v1_el, v2, v2_el, v3 = values
-    else:
-        p1, p2, p2_el, p3, p3_el, p4, p5, p6, p7, p8, p9, p10, p11, v1, \
-        v1_el, v2, v2_el, v3 = values
-    flag = p3.x
-    if len(p3_el.y) != 0:
-        safety_factor_el = v1_el.val / v2_el.val
-    else:
-        safety_factor_el = 0
-    fig = draw(x1=p1.x, y1=p1.y,
-               x2=p2_el.x, y2=p2_el.y,
-               x3=p3_el.x, y3=p3_el.y,
-               safety_factor=safety_factor_el,
-               flag=flag,
-               x4=p4.x, y4=p4.y,
-               x5=p7.x, y5=p7.y,
-               x6=p8.x, y6=p8.y,
-               x7=p9.x, y7=p9.y)
-    plot(fig, filename='ground_curve_basic.html')
